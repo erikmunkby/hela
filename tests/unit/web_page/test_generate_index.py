5ad8dfdf1ebb36b3761fbef6fe0c69b7912d6063
Erik Munkby <erik.munkby@gmail.com>
2022-02-09 09:53:27 +0100

    fix: index html file now zipped to reduce memory

---
+++ tests/unit/web_page/test_generate_index.py
@@ -0,0 +1,25 @@
+from catalog.datasets.pandas_parquet_dataset import PandasParquetDataset
+from catalog import Col, NestedCol, Catalog, generate_webpage
+from catalog.data_types import Int, String
+
+test_dataset = PandasParquetDataset(
+    name='test_dataset',
+    description='Test Dataset Structure.',
+    columns=[
+        Col('test_col', Int(), 'testcol'),
+        NestedCol('nested', [
+            Col('test_col2', String(), 'nested testcol'),
+            Col('filler_col', String(), 'filler col')
+        ])
+    ]
+)
+test_dataset._id = 'test_id'
+
+
+@Catalog.setup(description='Test Catalog Structure.')
+class TestCatalog(Catalog):
+    test_dataset = test_dataset
+
+
+def test_generate_index(temp_folder):
+    generate_webpage(TestCatalog, str(temp_folder))
